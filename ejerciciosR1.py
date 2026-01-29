bfdbd347d9f1efe5b6739e6f7faa837588bc2f08
Napo Lopez <104959346+NapoSV@users.noreply.github.com>
2026-01-29 15:22:13 -0600

    Add files via upload
    Agregar primer archivo de ejercicios de Programación 1. Este es relacionado a la creacion de variables y tipos de datos.

---
+++ ejerciciosR1.py
@@ -0,0 +1,84 @@
+# Guía de ejercicios de declaraciones Python Registro 1
+
+# Ejercicio 1
+# Declara una variable con un valor entero.
+numero_entero = 20
+
+# Usa la función type() para mostrar su tipo.
+print(type(numero_entero))
+
+# Ejercicio 2
+
+# Declara una variable con un valor negativo entero.
+numero_negativo_entero = -35
+
+# Verifica el tipo de la variable.
+print(type(numero_negativo_entero))
+
+# Ejercicio 3
+
+# Declara una variable con un valor flotante.
+numero_flotante = 35.33
+
+# Usa type() para verificar su tipo.
+print(type(numero_flotante))
+
+# Ejercicio 4
+
+# Declara un número flotante negativo
+numero_flotante_negativo = -0.96969
+
+# Verifica el tipo de variable
+print(type(numero_flotante_negativo))
+
+# Ejercicio 5
+
+# Declara una cadena de texto
+cadena_texto = "Programación 1: Un nuevo universo se está abriendo"
+
+# Usa type() para verificar el tipo de la variable.
+print(type(cadena_texto))
+
+# Ejercicio 6
+# Declara una variable con una cadena vacía.
+cadena_vacia = ""
+
+# cadena vacía.
+print(type(cadena_vacia))
+
+# Ejercicio 7
+
+# Declara un valor booleano verdadero.
+booleano_verdadero = True
+
+# Verifica el tipo de la variable.
+print(type(booleano_verdadero))
+
+# Ejercicio 8
+# Declara un valor booleano falso.
+booleano_falso = False
+
+# Verifica el tipo de la variable.
+print(type(booleano_falso))
+
+# Ejercicio 9
+
+#Declara variables de cada uno de los tipos: int, float, str y bool.
+varriable_int = 10
+variable_float = 0.5
+variable_str = "El Salvador"
+variable_bool = True
+
+# Verifica su tipo con type().
+# No se puede hacer una concatenacion de Type(). porque son objetos y en Python no se pueden sumar objetos type con +
+# Por eso he separado los print(type()) en diferentes print() y separados por comas ","
+print(type(varriable_int)), print(type(variable_float)), print(type(variable_str)), print(type(variable_bool))
+
+# Ejerciccio 10
+
+# Declara dos variables de tipos diferentes (por ejemplo: int y str).
+variable_bool2 = False
+variable_float2 = 3.1416
+
+# Verifica sus tipos con type().
+print(type(variable_bool2)), print(type(variable_float2))
